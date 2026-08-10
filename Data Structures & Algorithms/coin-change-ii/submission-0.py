31ad424b6b01316011041dbaa73dbd1a75ea1ec0
Anton <147023490+ChenxinZheng1128@users.noreply.github.com>
2026-08-10 11:26:17 -0400

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        # dp[a] stores the number of ways to make amount 'a'
+        dp = [0] * (amount + 1)
+        dp[0] = 1  # Base case: 1 way to make amount 0
+
+        for coin in coins:
+            for a in range(coin, amount + 1):
+                dp[a] += dp[a - coin]
+
+        return dp[amount]
